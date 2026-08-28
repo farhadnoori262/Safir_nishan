c86230947413ff6af48e70e43773b7e269deaa06
farhadnoori262 <noorifarhad262@gmail.com>
2026-08-28 23:58:55 +0330

    Update vehicle_registration_screen.dart

--- lib/pages/driverRegistration/vehicle_registration/vehicle_registration_screen.dart
+++ lib/pages/driverRegistration/vehicle_registration/vehicle_registration_screen.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
 
-import '../../providers/registration_provider.dart';
+import '../../../providers/registration_provider.dart';
 import '../../../utils/app_colors.dart';
 
 
