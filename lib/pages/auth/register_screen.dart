d55b5fc2fb402c381aa553f0899c8630bc6f66c4
farhadnoori262 <noorifarhad262@gmail.com>
2026-08-29 09:26:59 +0330

    Update register_screen.dart

--- lib/pages/auth/register_screen.dart
+++ lib/pages/auth/register_screen.dart
@@ -1,5 +1,5 @@
 import 'package:country_picker/country_picker.dart';
-import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization/easy_localization.dart' hide TextDirection;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
