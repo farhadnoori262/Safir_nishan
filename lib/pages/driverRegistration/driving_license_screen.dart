3e91dace92f004693df960ef8e1c79c8c2f0610f
farhadnoori262 <noorifarhad262@gmail.com>
2026-08-29 09:39:04 +0330

    Update driving_license_screen.dart

--- lib/pages/driverRegistration/driving_license_screen.dart
+++ lib/pages/driverRegistration/driving_license_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization/easy_localization.dart' hide TextDirection;
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:provider/provider.dart';
