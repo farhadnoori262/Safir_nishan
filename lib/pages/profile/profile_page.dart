298a4a314879ed7a4c3bc46d44769e2505d4607a
farhadnoori262 <noorifarhad262@gmail.com>
2026-08-29 09:40:01 +0330

    Update profile_page.dart

--- lib/pages/profile/profile_page.dart
+++ lib/pages/profile/profile_page.dart
@@ -1,5 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization/easy_localization.dart' hide TextDirection;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
