ee5f1fabda516403d226f5ef1a927d4819e8fbbc
farhadnoori262 <noorifarhad262@gmail.com>
2026-08-29 09:36:54 +0330

    Update basic_info_screen.dart

--- lib/pages/driverRegistration/basic_info_screen.dart
+++ lib/pages/driverRegistration/basic_info_screen.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 
-import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization/easy_localization.dart' hide TextDirection;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
