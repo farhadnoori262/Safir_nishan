93a078cce66b918637192c58c921d169e21cf70c
farhadnoori262 <noorifarhad262@gmail.com>
2026-08-30 10:29:35 +0330

    Update navigation_controls.dart

--- lib/pages/navigation/widgets/navigation_controls.dart
+++ lib/pages/navigation/widgets/navigation_controls.dart
@@ -1,67 +1,76 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
+import '../../../utils/app_colors.dart';
+
 class NavigationControls extends StatelessWidget {
-  final bool isNavigating;
-  final VoidCallback onResetNorth;
+  final bool navigationStarted;
+  final VoidCallback onResetToNorth;
   final VoidCallback onShowFullRoute;
   final VoidCallback onGoToStart;
-  final VoidCallback onMyLocation;
+  final VoidCallback onFollowLocation;
 
   const NavigationControls({
     super.key,
-    required this.isNavigating,
-    required this.onResetNorth,
+    required this.navigationStarted,
+    required this.onResetToNorth,
     required this.onShowFullRoute,
     required this.onGoToStart,
-    required this.onMyLocation,
+    required this.onFollowLocation,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        _MapControlButton(
-          tooltip: 'شمال',
-          icon: Icons.navigation_rounded,
-          iconColor: Colors.redAccent,
-          onPressed: onResetNorth,
+    return Positioned(
+      right: 14,
+      bottom: navigationStarted ? 132 : 290,
+      child: SafeArea(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _MapActionButton(
+              icon: Icons.navigation_rounded,
+              tooltip: 'align_to_north'.tr(),
+              iconColor: Colors.redAccent,
+              onPressed: onResetToNorth,
+            ),
+            const SizedBox(height: 10),
+            if (navigationStarted) ...[
+              _MapActionButton(
+                icon: Icons.alt_route_rounded,
+                tooltip: 'show_full_route'.tr(),
+                onPressed: onShowFullRoute,
+              ),
+              const SizedBox(height: 10),
+              _MapActionButton(
+                icon: Icons.home_outlined,
+                tooltip: 'back_to_start'.tr(),
+                onPressed: onGoToStart,
+              ),
+              const SizedBox(height: 10),
+            ],
+            _MapActionButton(
+              icon: Icons.my_location_rounded,
+              tooltip: 'my_location'.tr(),
+              iconColor: AppColors.primaryButton,
+              onPressed: onFollowLocation,
+            ),
+          ],
         ),
-        const SizedBox(height: 10),
-        if (isNavigating) ...[
-          _MapControlButton(
-            tooltip: 'نمایش کل مسیر',
-            icon: Icons.alt_route_rounded,
-            onPressed: onShowFullRoute,
-          ),
-          const SizedBox(height: 10),
-          _MapControlButton(
-            tooltip: 'بازگشت به آغاز مسیر',
-            icon: Icons.home_outlined,
-            onPressed: onGoToStart,
-          ),
-          const SizedBox(height: 10),
-        ],
-        _MapControlButton(
-          tooltip: 'موقعیت من',
-          icon: Icons.my_location_rounded,
-          iconColor: const Color(0xFF168A61),
-          onPressed: onMyLocation,
-        ),
-      ],
+      ),
     );
   }
 }
 
-class _MapControlButton extends StatelessWidget {
-  final String tooltip;
+class _MapActionButton extends StatelessWidget {
   final IconData icon;
-  final Color? iconColor;
+  final String tooltip;
   final VoidCallback onPressed;
+  final Color? iconColor;
 
-  const _MapControlButton({
-    required this.tooltip,
+  const _MapActionButton({
     required this.icon,
+    required this.tooltip,
     required this.onPressed,
     this.iconColor,
   });
@@ -71,13 +80,14 @@ class _MapControlButton extends StatelessWidget {
     return Material(
       color: Colors.white,
       elevation: 5,
+      shadowColor: Colors.black38,
       shape: const CircleBorder(),
       child: IconButton(
         tooltip: tooltip,
         onPressed: onPressed,
         icon: Icon(
           icon,
-          color: iconColor ?? const Color(0xFF202124),
+          color: iconColor ?? AppColors.textPrimary,
         ),
       ),
     );
