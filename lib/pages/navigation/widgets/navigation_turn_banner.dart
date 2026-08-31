d32cda7a8ffa5d069e9fba8ae0ffebfea0f928e0
farhadnoori262 <noorifarhad262@gmail.com>
2026-08-31 23:44:54 +0330

    Update navigation_turn_banner.dart

--- lib/pages/navigation/widgets/navigation_turn_banner.dart
+++ lib/pages/navigation/widgets/navigation_turn_banner.dart
@@ -21,10 +21,10 @@ class NavigationTurnBanner extends StatelessWidget {
 
     if (meters >= 1000) {
       final kilometers = meters / 1000;
-      return '${kilometers.toStringAsFixed(1)} ${'kilometers'.tr()}';
+      return '${kilometers.toStringAsFixed(1)} کیلومتر';
     }
 
-    return '$meters ${'meters'.tr()}';
+    return '$meters متر';
   }
 
   String _streetText() {
@@ -88,6 +88,7 @@ class NavigationTurnBanner extends StatelessWidget {
                         _distanceText(),
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
+                        textDirection: ui.TextDirection.rtl,
                         style: const TextStyle(
                           color: AppColors.buttonText,
                           fontSize: 17,
