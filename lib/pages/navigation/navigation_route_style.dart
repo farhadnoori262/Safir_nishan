db781c5cf4f52674aef91b70c8c631e5127df081
farhadnoori262 <noorifarhad262@gmail.com>
2026-09-01 00:04:13 +0330

    Update navigation_route_style.dart

--- lib/pages/navigation/navigation_route_style.dart
+++ lib/pages/navigation/navigation_route_style.dart
@@ -1,33 +1,36 @@
 import 'package:maplibre_gl/maplibre_gl.dart';
+
 class NavigationRouteStyle {
   NavigationRouteStyle._();
-  static String get currentMapStyle {
-    final hour = DateTime.now().hour;
-    final isNight = hour >= 19 || hour < 6;
-    return isNight
-        ? 'https://tiles.stadiamaps.com/styles/alidade_smooth_dark.json'
-        : 'assets/map/style.json';
-  }
+
+  // همیشه استایل روز بارگذاری می‌شود
+  static String get currentMapStyle => 'assets/map/style.json';
+
   static Future<void> drawRoute(
     MapLibreMapController controller,
     List<LatLng> points,
   ) async {
     if (points.length < 2) return;
+
     await controller.clearLines();
+
     await controller.addLine(
       LineOptions(
         geometry: points,
-        lineColor: '#07553D',
+        lineColor: '#000000',
         lineWidth: 18.0,
         lineOpacity: 0.96,
+        lineJoin: 'round',
       ),
     );
+
     await controller.addLine(
       LineOptions(
         geometry: points,
-        lineColor: '#19B879',
+        lineColor: '#00B4D8',
         lineWidth: 11.0,
         lineOpacity: 1.0,
+        lineJoin: 'round',
       ),
     );
   }
