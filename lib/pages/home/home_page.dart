e9a22e75dcc4731086b66beb3d7c5b1a1d32a668
farhadnoori262 <noorifarhad262@gmail.com>
2026-09-02 01:19:10 +0330

    Update home_page.dart

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -8,9 +8,8 @@ import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:safir_drivers/pages/chat_page.dart';
 
-import 'package:safir_drivers/pages/chat/chat_page.dart'; // 📌 مسیر فایل ChatPage خود را چک کنید
+import 'package:safir_drivers/pages/chat_page.dart'; // 📌 مسیر فایل ChatPage خود را چک کنید
 import 'package:safir_drivers/providers/registration_provider.dart'; 
 import 'package:safir_drivers/utils/app_colors.dart';
 import '../../push_notifications/push_notification_system.dart';
