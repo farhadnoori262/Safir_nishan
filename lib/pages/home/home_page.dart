769dae39e2356b776470ec10596b5c4b155fe8bb
farhadnoori262 <noorifarhad262@gmail.com>
2026-09-06 23:02:51 +0330

    Update home_page.dart

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -151,7 +151,7 @@ class _HomePageState extends State<HomePage> {
     final user = FirebaseAuth.instance.currentUser;
     if (user == null) return;
     String uid = user.uid;
-    
+    driverOnlineTimestamp = DateTime.now();
 
     currentPositionOfDriver ??= await getCurrentLiveLocationOfDriver();
     if (currentPositionOfDriver != null) {
